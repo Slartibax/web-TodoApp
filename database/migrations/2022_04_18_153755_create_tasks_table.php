be56f94124955f72238b83a078fae23c4c5086a5
Slartibax <slarti.bartfast@mail.ru>
2022-04-18 23:09:54 +0700

    Добавил и исполнил миграции для MySql.

---
+++ database/migrations/2022_04_18_153755_create_tasks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('project_id');
+            $table->boolean('is_complete');
+            $table->string('task_name',70);
+            $table->date('schedule_date');
+            $table->string('task_description',250);
+            $table->timestamps();
+
+            $table->foreign('project_id')
+                ->references('id')->on('projects')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
