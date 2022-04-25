dcc0ce4c312d2c7db6ea1c6d8655fb0e95d8936e
Slartibax <slarti.bartfast@mail.ru>
2022-04-25 18:02:57 +0700

    Теперь данные для отображения dashboard передаются через методы отношений в моделях. Теперь если пользователь попробует перейти на недоступный сегмент проекта, его перенаправвит на первый доступный ему проект.

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,21 +7,28 @@ use App\Models\User;
 use App\Models\Project;
 use App\Models\User_Project;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 use Illuminate\View\View;
+use phpDocumentor\Reflection\Types\Null_;
 
 
 class DashboardController extends Controller
 {
-    public function show($projectId)
+    public function show(Request $request)
     {
-        $user = User::query()->where('id','=',3)->first();
-        $project = Project::query()->where('id', $projectId)->first();
+        $user = User::query()->where('id',Auth::id())->first();
+        $project = Project::query()->where('id', $request->project)->first();
 
-        $personal = User_Project::getOptionsForUser($user);
-        $shared = User_Project::getOptionsForUser($user);
-        $members = User_Project::getMembersForProject($project);
-        $options = ['personal' => $personal->all(), 'shared' => $shared];
-        $project = [ 'head' => $project, 'days' => $project->getSortedDays()];
+        if ($project == NULL){
+            return redirect()->route('dashboard.show',['project'=>$user->projects()->first()->id]);
+        }
+
+        $personal = $user->projects;
+        $shared = $user->projects;
+        $members = $project->members;
+        $options = ['personal' => $personal, 'shared' => $shared];
+        $project = [ 'head' => $project, 'days' => $project->sortedDays()];
 
         //TODO Сделать нормальную передачу данных
         $data = ['user' => $user,
@@ -29,6 +36,7 @@ class DashboardController extends Controller
             'options' => $options,
             'members' => $members
         ];
-        return view('dashboard',)->with('data',$data);
+//        return dd($data);
+        return view('dashboard')->with('data', $data);
     }
 }
